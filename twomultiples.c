39099e6032d2565a6be39c48e169428a4d515a36
Rasamsetticharan <84368177+Rasamsetticharan@users.noreply.github.com>
2021-05-18 19:16:36 +0530

    Add files via upload

---
+++ twomultiples.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int num,i,r1,r2,t;
+	scanf("%d%d%d",&num,&r1,&r2);//5 10 3
+	//swapping of two numbers logic
+	if(r1>r2)//10>3
+	{
+		t=r1;//t=10 r1=10 r2=3
+		r1=r2;//t=10 r1=3 r2=3
+		r2=t;//t=10 r1=3 r2=10
+	}
+	for(i=r1;i<=r2;i++)//10<=3
+	{
+		printf
+	}
+}
